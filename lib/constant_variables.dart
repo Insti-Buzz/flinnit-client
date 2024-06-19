cfa5db71f650d65f4284cfdcce4dda42aa1eb8a9
instibuzz <jujhar.channa@gmail.com>
2024-06-19 22:36:21 +0530

    latest update

---
+++ lib/constant_variables.dart
@@ -0,0 +1,1 @@
+final backendUri = 'https://0d64-103-158-43-34.ngrok-free.app';
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,33 @@
-import 'package:flinnit/models/chat_users.dart';
-import 'package:flinnit/screens/tabs.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flinnit/firebase_options.dart';
+import 'package:flinnit/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message: ${message.messageId}');
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print(fcmToken);
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TabsScreen(
-        myName: 'Jujhar Singh Channa',
-        myGender: Gender.male,
-      ),
+      home: LoginPage(),
     ),
   );
 }
